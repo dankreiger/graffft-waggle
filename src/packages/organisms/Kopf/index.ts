8f21da0b19fbf5dee1d3e2055feab53c6a691e36
Dan Kreiger <dankreiger@gmail.com>
2022-06-05 00:23:54 +0200

    fix: stuff (#311)
    * fix: stuff
    
    * fix: stuff

--- src/packages/organisms/Kopf/index.ts
+++ src/packages/organisms/Kopf/index.ts
@@ -1,10 +1,1 @@
-import { NavItem } from '@graffft-waggle/expandable-side-nav';
-
-import { Kopf } from './Kopf';
-import { KopfProps as props } from './types/Kopf.interfaces';
-import { KopfHeaderLogoPostion } from './types/Kopf.enums';
-
-// used for build script
-export type KopfProps = props;
-export type KopfHeaderNavItem = NavItem;
-export { Kopf, KopfHeaderLogoPostion };
+export * from './Kopf';
