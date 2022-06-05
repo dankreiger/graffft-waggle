f82badd1301cc3e75778854ecad961dc2c2a8cb1
Dan Kreiger <dankreiger@gmail.com>
2022-06-05 21:03:02 +0200

    fix: update snapshot

--- src/packages/organisms/Kopf/hooks.ts
+++ src/packages/organisms/Kopf/hooks.ts
@@ -43,7 +43,6 @@ export const useKopfDefaults = (props: KopfProps) => {
     ) {
       kopfProps = { ...kopfProps, ...extra };
     }
-    console.log(kopfProps);
     return kopfProps;
   }, [props, extra]);
 };
